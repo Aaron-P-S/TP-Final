b98fcfa08d4bd906dd84fb37b6172415156bbc6e
PedroFR00 <134623626+PedroFR00@users.noreply.github.com>
2025-11-07 19:42:58 -0300

    Modificaciones del menu

--- TP Final Prueba/src/Modelo/Gestora.java
+++ TP Final Prueba/src/Modelo/Gestora.java
@@ -6,19 +6,32 @@ public class Gestora {
     //Coleccion donde guardamos las diferentes partidas
 
     //Metodos para ejecutar el juego
-    public void menu(){
+    public void menu() {
         Scanner sc = new Scanner(System.in);
         boolean continuar = true;
         int eleccion = 0;
         while (continuar) {
             switch (eleccion) {
-                case 1:
-
-                    eleccion = 0;
+                case 0:
+                    System.out.println("Bienvenido al juego ..." +
+                            "Ingrese un numero para continuar:" +
+                            "1. Empezar pelea:" +
+                            "2. Visitar la tienda" +
+                            "3. Salir");
+                    eleccion = sc.nextInt();
+                    sc.nextLine();
                     break;
-                    default:
-                        System.out.println("Bienvenido al juego ...");
-                        break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    //guardar los datos antes de salir
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Se ingreso un numero no valido, intentelo de nuevo");
+                    break;
             }
         }
     }
