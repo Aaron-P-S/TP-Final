aac0be4619f0b3f64fbd07ace6166509967cd200
PedroFR00 <134623626+PedroFR00@users.noreply.github.com>
2025-11-10 12:56:04 -0300

    Update Menu.java

--- TP Final Prueba/src/Modelo/Menu.java
+++ TP Final Prueba/src/Modelo/Menu.java
@@ -156,6 +156,7 @@ public class Menu {
                         turno++;
                     } else {
                         System.out.println("+-----------------------🤺---------------------------+");
+                        System.out.println(partida.vidaParty());
                         int valorAtaque = partida.party.get(turno).atacar(partida.enemigos.get(nivel));
                         System.out.println(partida.party.get(turno).getNombre() + " atacara por " + valorAtaque + "☄️");
                         System.out.println(partida.enemigos.get(nivel).getNombre() + " tiene " + partida.enemigos.get(nivel).getPuntosDeVidaActual() + " ❤️ puntos de vida restantes ️");
